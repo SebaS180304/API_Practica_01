9c9b50fd0156c95e56667f37deb78d69eb81dc16
SebaS180304 <drdiegoramos18@gmail.com>
2026-04-09 14:42:16 -0600

    Revert "Initial Express API scaffold and user CRUD"
    This reverts commit f4b11185f4dba6aeff453879d6296f50185c22d6.

--- models/users.model.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const userSchema = mongoose.Schema({
-    name: String,
-    username: { type: String, unique: true },
-    password: String
-}, { timestamps: true });
-
-export default mongoose.model("User", userSchema);
